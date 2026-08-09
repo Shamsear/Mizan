5bb484fb10f828eb60d399aea960deaa92535e81
Shamsear <4pa20cs104@pace.edu.in>
2026-08-09 12:32:29 +0300

    ewq

--- src/components/PageTransition/PageTransition.tsx
+++ src/components/PageTransition/PageTransition.tsx
@@ -11,7 +11,7 @@ export function PageTransition({ children }: { children: ReactNode }) {
   const pathname = usePathname();
 
   return (
-    <div key={pathname} className="page-enter">
+    <div key={pathname} className="route-transition">
       {children}
     </div>
   );
